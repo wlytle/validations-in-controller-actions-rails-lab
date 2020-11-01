7cd6796fdf854bdbafb04379366c700d971882fe
wlytle <wlytle89@gmail.com>
2020-11-01 12:04:56 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Category must be either Fiction or Non-fiction" }
+  validates :content, length: { minimum: 100 }
 end
